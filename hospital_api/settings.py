90e678d5974c9ae5907b0ec7ad458f8b3b598f72
thatquietkid <nitinch131@gmail.com>
2025-07-02 01:17:39 +0530

    hosts changes

--- hospital_api/settings.py
+++ hospital_api/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-u+r0*@t&!#do@nd7(x=9w%807&4lbr1u^&=)8^ck41c^1fz6fi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 from datetime import timedelta
